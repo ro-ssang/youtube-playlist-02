43aff7c0503a51537cde33d565f1406e7149c237
SangRo Lee <fashionfish@naver.com>
2021-11-06 10:01:16 +0900

    Feat: LoggedInLayout으로 컴포넌트 분리

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,54 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import LoginHome from '../templates/LoginHome';
 import LogoutHome from '../templates/LogoutHome';
-import { login, logout } from '../../store/auth';
-import { setProfile, getPlaylists } from '../../store/user';
-import { getPopularVideos } from '../../store/videos';
+import { login } from '../../store/auth';
 
-function Home({
-  isLogin,
-  login,
-  logout,
-  profile,
-  setProfile,
-  loadingPlaylists,
-  playlists,
-  getPlaylists,
-  loadingPopularVideos,
-  popularVideos,
-  getPopularVideos,
-}) {
-  return isLogin ? (
-    <LoginHome
-      logout={logout}
-      profile={profile}
-      setProfile={setProfile}
-      loadingPlaylists={loadingPlaylists}
-      playlists={playlists}
-      getPlaylists={getPlaylists}
-      loadingPopularVideos={loadingPopularVideos}
-      popularVideos={popularVideos}
-      getPopularVideos={getPopularVideos}
-    />
-  ) : (
-    <LogoutHome login={login} />
-  );
+function Home({ isLogin, login }) {
+  return isLogin ? <LoginHome /> : <LogoutHome login={login} />;
 }
 
 export default connect(
-  ({ auth, user, videos }) => ({
+  ({ auth }) => ({
     isLogin: auth.isLogin,
-    profile: user.profile,
-    loadingPlaylists: user.loading.PLAYLISTS,
-    playlists: user.playlists,
-    loadingPopularVideos: videos.loading.GET_POPULAR_VIDEOS,
-    popularVideos: videos.popularVideos,
   }),
   {
     login,
-    logout,
-    setProfile,
-    getPlaylists,
-    getPopularVideos,
   }
 )(Home);
--- src/components/templates/LoginHome.jsx
+++ src/components/templates/LoginHome.jsx
@@ -1,32 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import SectionTitle from '../atoms/SectionTitle';
-import Aside from '../modules/Aside';
-import Main from '../atoms/Main';
 import VideoItem from '../modules/VideoItem';
-import AddPlayItem from '../atoms/AddPlayItem';
-import AuthBox from '../atoms/AuthBox';
-import BrowseItem from '../atoms/BrowseItem';
-import ListTitle from '../atoms/ListTitle';
-import PlayItem from '../atoms/PlayItem';
-import SiteLogo from '../atoms/SiteLogo';
-import SearchForm from '../modules/SearchForm';
-import LogoutBox from '../modules/LogoutBox';
 import Loader from '../atoms/Loader';
+import LoggedInLayout from './LoggedInLayout';
+import { connect } from 'react-redux';
 
-const Wrapper = styled.div`
-  display: grid;
-  height: 100vh;
-  grid-template-columns: ${({ theme }) => theme.sizes.sidebar.width} 1fr;
-  grid-template-rows: 100%;
-`;
-const Nav = styled.nav`
-  padding: 0px 1.5625rem;
-  margin: 1rem 0px;
-  overflow: auto;
-`;
-const BrowseList = styled.ul``;
-const PlayList = styled.ul``;
 const Section = styled.section`
   padding: 2rem 2.5rem 0px;
 `;
@@ -36,76 +15,39 @@ const VideoList = styled.ul`
   flex-wrap: wrap;
 `;
 
-function LoginHome({
-  logout,
-  profile,
-  setProfile,
-  loadingPlaylists,
-  playlists,
-  getPlaylists,
-  loadingPopularVideos,
-  popularVideos,
-  getPopularVideos,
-}) {
-  useEffect(() => {
-    getPopularVideos();
-    getPlaylists();
-  }, [getPopularVideos, getPlaylists]);
-
+function LoginHome({ loadingPopularVideos, popularVideos }) {
   return (
-    <Wrapper>
-      <Aside>
-        <SiteLogo isAside={true} />
-        <SearchForm />
-        <Nav>
-          <BrowseList>
-            <BrowseItem />
-          </BrowseList>
-          <ListTitle>플레이리스트</ListTitle>
-          <PlayList>
-            <AddPlayItem />
-            {loadingPlaylists && <Loader />}
-            {!loadingPlaylists &&
-              playlists &&
-              playlists.map((item) => {
+    <LoggedInLayout>
+      <Section>
+        <SectionTitle>인기 뮤직 비디오</SectionTitle>
+        {loadingPopularVideos && <Loader />}
+        {!loadingPopularVideos && popularVideos && (
+          <>
+            <VideoList>
+              {popularVideos.map((video, index) => {
                 const {
                   id,
-                  snippet: { title },
-                } = item;
-                return <PlayItem key={id} text={title} path={`/playlist/${id}`} />;
-              })}
-          </PlayList>
-        </Nav>
-        <AuthBox>
-          <LogoutBox profile={profile} setProfile={setProfile} logoutPath="/" logout={logout} />
-        </AuthBox>
-      </Aside>
-      <Main>
-        <Section>
-          <SectionTitle>인기 뮤직 비디오</SectionTitle>
-          {loadingPopularVideos && <Loader />}
-          {!loadingPopularVideos && popularVideos && (
-            <>
-              <VideoList>
-                {popularVideos.map((video, index) => {
-                  const {
-                    id,
-                    snippet: {
-                      title,
-                      thumbnails: {
-                        medium: { url: thumbnail },
-                      },
+                  snippet: {
+                    title,
+                    thumbnails: {
+                      medium: { url: thumbnail },
                     },
-                  } = video;
-                  return <VideoItem key={id} title={title} thumbnail={thumbnail} rank={index + 1} />;
-                })}
-              </VideoList>
-            </>
-          )}
-        </Section>
-      </Main>
-    </Wrapper>
+                  },
+                } = video;
+                return <VideoItem key={id} title={title} thumbnail={thumbnail} rank={index + 1} />;
+              })}
+            </VideoList>
+          </>
+        )}
+      </Section>
+    </LoggedInLayout>
   );
 }
 
-export default LoginHome;
+export default connect(
+  ({ videos }) => ({
+    loadingPopularVideos: videos.loading.GET_POPULAR_VIDEOS,
+    popularVideos: videos.popularVideos,
+  }),
+  {}
+)(LoginHome);
