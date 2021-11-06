39c9b487ae98aa10c08ad42b8e9f20e4780ef211
SangRo Lee <fashionfish@naver.com>
2021-11-06 11:18:35 +0900

    Chore: Search 컴포넌트 분리

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import LoginSearch from '../templates/LoginSearch';
+import styled from 'styled-components';
+import SectionTitle from '../atoms/SectionTitle';
+import VideoItem from '../modules/VideoItem';
+
+const Section = styled.section`
+  padding: 2rem 2.5rem 0px;
+`;
+const VideoList = styled.ul`
+  display: flex;
+  margin: 1.5rem auto 0px;
+  flex-wrap: wrap;
+`;
 
 function Search() {
-  return <LoginSearch />;
+  return (
+    <Section>
+      <SectionTitle>검색 결과</SectionTitle>
+      <VideoList>
+        <VideoItem
+          title="strawberry moon (strawberry moon)"
+          thumbnail="https://i.ytimg.com/vi/Kevp2lFKSOg/mqdefault.jpg"
+          rank="1"
+        />
+      </VideoList>
+    </Section>
+  );
 }
 
 export default Search;
