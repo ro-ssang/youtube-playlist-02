c338c4cb09ef6573f9e739f78603115512ee37aa
rossa <fashionfish@naver.com>
2021-11-18 21:55:44 +0900

    chore. client id 변경

--- src/contants/index.js
+++ src/contants/index.js
@@ -1,4 +1,4 @@
 export const LS_TOKEN = 'token';
 export const LS_PROFILE = 'profile';
-export const CLIENT_ID = '478590049856-de58rj0tomsahk0tpvq925st7jahf2tk.apps.googleusercontent.com';
+export const CLIENT_ID = '478590049856-npgjq5tg2u9k4ogkmrqmhvdo63jr9og7.apps.googleusercontent.com';
 export const YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3';
